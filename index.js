f8553ce6e755caa1d0e288eaf09c2671d9e83667
Edilene Da Cruz <hello@edilene.io>
2017-10-24 05:22:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(number) {
   var crazyFan = [];
-}
+
   do { crazyFan.push("I love the Beatles!"); number++;
 } while (number < 15);
 return crazyFan
+}
