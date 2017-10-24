db0ff38d60bd8c1950345cc5eaccd5a9eec07252
Edilene Da Cruz <hello@edilene.io>
2017-10-24 05:05:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var band = []
-  for (let i = 0, i < musicians.length, i++) 
+  for (let i = 0, i < musicians.length, i++) {
+    console.log(`${musicians[i]} plays ${instruments[i]}`)
+  }
 }
